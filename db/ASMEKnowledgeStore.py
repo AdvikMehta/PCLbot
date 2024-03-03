31432c0f8f69796810ab03438166b9ca909af1c5
AdvikMehta <advikmehta444@yahoo.com>
2024-03-02 19:03:43 -0700

    added pinecone support

---
+++ db/ASMEKnowledgeStore.py
@@ -0,0 +1,69 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.documents import Document
+from langchain_core.vectorstores import VectorStore
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores.pinecone import Pinecone
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+class ASMEKnowledgeStore:
+
+    def __init__(self, index_name: str, init_doc_paths: list = None):
+        load_dotenv()
+        self.index_name = index_name
+        self.embeddings = OpenAIEmbeddings()
+
+        if init_doc_paths is not None:
+            self.vectordb: VectorStore = self.__init_index_with_docs(
+                self.index_name,
+                self.embeddings,
+                init_doc_paths,
+            )
+        else:
+            self.vectordb = Pinecone.from_existing_index(
+                index_name=self.index_name,
+                embedding=self.embeddings
+            )
+
+    def add_docs(self, doc_paths: list[str]):
+        """
+        Adds docs into a pinecone index
+        """
+        self.vectordb.add_documents(self.__process_pdfs(doc_paths))
+
+    def similarity_search(self, query: str, k: int = 4, search_filter: dict = None):
+        """
+        Performs a similarity search on the Vector Database and returns the top match
+        """
+        docs = self.vectordb.similarity_search(query, k, search_filter)
+        return docs[0].page_content
+
+    def clear_db(self):
+        self.vectordb.delete(delete_all=True)
+
+    def __init_index_with_docs(self, index_name, embeddings, init_doc_paths) -> VectorStore:
+        """
+        Initializes a Pinecone index with input PDF docs
+        """
+        return PineconeVectorStore.from_documents(
+            documents=self.__process_pdfs(init_doc_paths),
+            embedding=embeddings,
+            index_name=index_name)
+
+    def __process_pdfs(self, init_doc_paths) -> list[Document]:
+        """
+        Processs PDFs - loads and splits documents
+        """
+        docs = []
+        for path in init_doc_paths:
+            # init loader and load data
+            loader = PyPDFLoader(path)
+            data = loader.load()
+
+            # split into chunks
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+            docs += text_splitter.split_documents(data)
+
+        return docs
+
---
+++ test/TestASMEKnowledgeStore.py
@@ -0,0 +1,17 @@
+from db.ASMEKnowledgeStore import ASMEKnowledgeStore
+
+pdf_path = "../test/data/marvel_prop_auction.pdf"
+pdf_path_2 = "../test/data/golf_guidebook.pdf"
+
+index_name = "asme-bot-knowledge"
+
+query = "How much is the Elektra Natchios’ Overcoat and Pair of Stunt Wakizashis listed for?"
+query2 = "What did Shot Scope find about golfers who missed their tee shots to the right?"
+
+vectordb = ASMEKnowledgeStore(index_name)
+# print(vectordb.similarity_search(query))
+# print(vectordb.similarity_search(query2))
+
+# print(vectordb.add_docs([pdf_path_2]))
+# print(vectordb.similarity_search(query2))
+vectordb.clear_db()
