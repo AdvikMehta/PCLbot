a50a38182af7d693b06dd8d5e18e0c08b195e20e
AdvikMehta <advikmehta444@yahoo.com>
2024-03-20 18:15:33 -0600

    Implemented the 'ftrag' mode in the benchamrk, using the Helix API Dynamic chunking and embedding of documents

--- db/ASMEKnowledgeStore.py
+++ db/ASMEKnowledgeStore.py
@@ -67,3 +67,4 @@ class ASMEKnowledgeStore:
 
         return docs
 
+
--- test/benchmark_test.py
+++ test/benchmark_test.py
@@ -1,10 +1,27 @@
 from benchmark.benchmark import Benchmark
+import pandas as pd
 
-benchmark = Benchmark("baserag")
+def read_csv(file_path):
+    df = pd.read_csv(file_path)
+    df.fillna('', inplace=True)
+    return [(question, answer) for question, answer in zip(df.iloc[:, 0], df.iloc[:, 1]) if question and answer]
 
-out = benchmark.run([
-    ("According to ASME B31.3, what is the minimum design metal temperature (in °F) for carbon steel without impact testing?", "-20 F")
-])
+def add_column_to_csv(file_path, column_name, data_list):
+    df = pd.read_csv(file_path)
+    # if len(data_list) != len(df):
+    #     raise ValueError('Length of new column data does not match number of rows in the existing CSV')
 
-print(out)
+    df[column_name] = data_list
+    df.to_csv(file_path, index=False)
+
+
+benchmark = Benchmark("ftrag")
+qa_pairs = read_csv("qa_pairs_with_mode_outputs.csv")[0:5]
+print(qa_pairs)
+
+outputs = benchmark.run(qa_pairs)
+print(outputs)
+
+# add_column_to_csv("qa_pairs_with_mode_outputs.csv", "base", outputs_base)
+# add_column_to_csv("qa_pairs_with_mode_outputs.csv", "baserag", outputs_baserag)
 
